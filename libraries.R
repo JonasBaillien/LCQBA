2db1c532f8f7e2dfed6358e677ea0e7ca91ba9bd
jonasbaillien <72913968+jonasbaillien@users.noreply.github.com>
2022-02-04 13:11:04 +0100

    Create libraries.R

---
+++ libraries.R
@@ -0,0 +1,18 @@
+### libraries ###
+#################
+
+library(DAAG)
+library(sn)
+library(mrfDepth)
+library(fields)
+library(doParallel)
+library(combinat)
+library(nloptr)
+library(optimx)
+library(utils)
+library(mvnTest)
+library(mixtools)
+library(LaplacesDemon)
+library(QBAsyDist)
+library(expm)
+library(alabama)
