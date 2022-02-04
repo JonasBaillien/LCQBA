9cb4708281713ca20d2d05caa1f388fb98e46285
jonasbaillien <72913968+jonasbaillien@users.noreply.github.com>
2022-02-04 10:38:10 +0100

    Update Code part 2 Simulation .R

--- Code part 2 Simulation .R
+++ Code part 2 Simulation .R
@@ -7,7 +7,10 @@
 # Please do so when running the simulation. The written output is later also 
 # read in again, please also change the correct directory there when needed.
 
+# this is the directory where everything is saved. 
 setwd(dir = choose.dir())
+
+# If the file "Code part 1 Functions.R" is not saved in the previously set directory, add the correct path.
 source("Code part 1 Functions.R")
 
 
@@ -192,10 +195,10 @@ result=foreach(i=1:reps,.packages=c('combinat','mrfDepth','nloptr',"optimx"),
                  start.time <- Sys.time()
                  
                  # fitting the model to the data
-                 secondattempt=MVQBAFD_fit(data = X,basefunc = basefunc,seed = seed+i,maxiter = maxiter,tol = tol,numstarts=numstarts)
+                 fit=MVQBAFD_fit(data = X,basefunc = basefunc,seed = seed+i,maxiter = maxiter,tol = tol,numstarts=numstarts)
                  
                  
-                 fits=c(as.matrix(secondattempt$`fitted parameters`,nrow=1),secondattempt$`log likelihood fit`)
+                 fits=c(as.matrix(fit$`fitted parameters`,nrow=1),fit$`log likelihood fit`)
                  
                  # tracking runtime
                  end.time <- Sys.time()
@@ -334,145 +337,6 @@ M[i,(2*dims+1):(2*dims+dims^2)]=Af
 sorted$manual
 # optional save for later use
 save(M,file=paste0("~/D",dims,"S",sampsize,"sorted.Rdata"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-###########################################################
-### script to fit all possible models on a real dataset ###
-###########################################################
-
-# please select the correct data and change the directories where output is written
-
-
-### load in the correct data ###
-################################
-
-# read in the Pokemon data
-Pokemon <- read.csv("~/Pokemon.csv", header=FALSE, comment.char="#") 
-Pokemondata=as.matrix(Pokemon[,6:11])
-dat=as.matrix(Pokemondata)
-
-# # load ais data
-# library(DAAG)
-# dat=as.matrix(ais[,c(6,9)])
-
-
-
-### matrix of all possible combinations of base functions ###
-#############################################################
-d=length(dat[1,])
-bf=c("normal","t","laplace","logistic")
-expandlist=list()
-for(i in 1:d){
-  expandlist[[i]]=bf
-}
-basefuncs=as.matrix(expand.grid(expandlist))
-basefuncs=apply(basefuncs,1,sort)
-basefuncs=t(basefuncs)
-basefuncs=unique(basefuncs,margin=1)
-
-nmods=length(basefuncs[,1])
-
-
-### create folder to store output in (put correct name here) ###
-################################################################
-dir.create(paste0("~/pokemon"))
-
-# also change the file locations further below to write in this directory
-
-
-
-### fitting the models to the data ###
-######################################
-
-# for the fitter
-seed=126
-maxiter=50000
-tol=10^-5
-maxstarts=50
-reqfits=25
-
-# main loop 
-cores=detectCores()
-cl <- makeCluster(4)
-registerDoParallel(cl)
-
-result=foreach(i=1:nmods,.packages=c('nloptr','optimx'),
-               .combine = "cbind",.verbose = T,.errorhandling="remove") %dopar% {
-                 
-                 # required files
-                 source("Code part 1 Functions.R")
-                 
-                 # basefunctions of the model fitted to the data
-                 basefunc=basefuncs[i,]
-                 
-                 # fitting the model to the data
-                 fit=MVQBAFD_fit2(data=dat,basefunc=basefunc,seed=seed,maxiter=maxiter,tol=tol,maxstarts=maxstarts,reqfits=reqfits)
-
-                 output=c(as.numeric(fit$`fitted parameters`),fit$`log likelihood fit`)
-                 
-                 # storing the output, put the right directory here (change the last file location)
-                 filename=paste0("~/pokemon/M",i,".csv")
-                 write.csv(output,file=filename)
-               }
-stopCluster(cl)
-x11()
-
-
-
-### combining output in list containing matrices for alpha, mu, A, ###
-### df, log-likelihood and base functions                          ###
-######################################################################
-
-alpha_fit=matrix(NA,nrow=nmods,ncol=d)
-mu_fit=matrix(NA,nrow=nmods,ncol=d)
-A_fit=matrix(NA,nrow=nmods,ncol=d^2)
-df_fit=matrix(NA,nrow=nmods,ncol=d)
-loglikelihood_fit=rep(NA,nmods)
-basefunctions_fit=basefuncs
-
-for (i in 1:nmods){
-  # put the right directory here (change the last file location)
-  temp_data = t(read.csv(paste0("~/pokemon/M",i,".csv"), row.names=1))
-  
-  alpha_fit[i,] = as.numeric(temp_data[1:d])
-  
-  mu_fit[i,] = as.numeric(temp_data[(d+1):(2*d)])
-  
-  A_fit[i,] = as.numeric(temp_data[(2*d+1):(d^2+2*d)])
-  
-  if(is.element("t",basefuncs[i,])){
-    indt=which(basefuncs[i,]=="t")
-    nt=length(indt)
-    df_fit[i,indt] = as.numeric(temp_data[(d^2+2*d+1):(d^2+2*d+nt)])
-  } else {
-    nt=0
-  }
-  
-  loglikelihood_fit[i] = as.numeric(temp_data[d^2+2*d+nt+1])
-}
-
-# combine output
-modelfit=list("alpha"=alpha_fit,"mu"=mu_fit,"A"=A_fit,"df"=df_fit,"logl"=loglikelihood_fit,"basefunc"=basefunctions_fit)
-
-# save output in correct location (change directory name if needed)
-save(modelfit,file="~/pokemon/fittedmodels_pokemon.Rdata")
-
 
 
 
